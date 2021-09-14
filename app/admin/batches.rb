acb9013f14eab7e99e2dbac97ec779f26488e92b
stemcell <marc.lechuga@inserm.fr>
2021-09-14 13:46:46 +0200

    bux fixed upload batches + display vials from batches

--- app/admin/batches.rb
+++ app/admin/batches.rb
@@ -1,5 +1,13 @@
 ActiveAdmin.register Batch do
 
+#Import csv   
+ active_admin_import validate: false,
+              csv_options: {col_sep: ";" },
+              before_batch_import: ->(importer) {   
+                Vial.where(id: importer.values_at('id')).delete_all
+              }
+
+
 csv force_quotes: false, col_sep: ';', column_names: true do
   column :id
   column :name
