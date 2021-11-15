b15458c202bc72af967c1c004ffb895626fe3c33
stemcell <marc.lechuga@inserm.fr>
2021-11-15 15:55:35 +0100

    vials 4

--- app/admin/boxes.rb
+++ app/admin/boxes.rb
@@ -6,18 +6,9 @@ ActiveAdmin.register Box do
                 Box.where(id: importer.values_at('id')).delete_all
               }
 
-
-# See permitted parameters documentation:
-# https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-#
-# permit_params :list, :of, :attributes, :on, :model
-#
-# or
-#
-# permit_params do
-#   permitted = [:permitted, :attributes]
-#   permitted << :other if params[:action] == 'create' && current_user.admin?
-#   permitted
-# end
+csv force_quotes: false, col_sep: ';', column_names: true do
+  column :id
+  column :name
+end
 
 end
