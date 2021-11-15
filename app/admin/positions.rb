61de04f708005bc0c8aaabdd49e03f4c5169d57a
stemcell <marc.lechuga@inserm.fr>
2021-11-15 11:35:44 +0100

    positions 3

--- app/admin/positions.rb
+++ app/admin/positions.rb
@@ -4,7 +4,7 @@ ActiveAdmin.register Position do
  active_admin_import validate: false,
               csv_options: {col_sep: ";" },
               before_batch_import: ->(importer) {   
-                Prosition.where(id: importer.values_at('id')).delete_all
+                Position.where(id: importer.values_at('id')).delete_all
               }
   
 csv force_quotes: false, col_sep: ';', column_names: true do
