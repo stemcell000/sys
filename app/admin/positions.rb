2bdf62c4c4ce0fdaa63db7ac7f0bc8fa012e2f29
stemcell <marc.lechuga@inserm.fr>
2021-11-15 11:23:42 +0100

    positions

--- app/admin/positions.rb
+++ app/admin/positions.rb
@@ -1,18 +1,10 @@
 ActiveAdmin.register Position do
 
-  # See permitted parameters documentation:
-  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-  #
-  # Uncomment all parameters which should be permitted for assignment
-  #
-  # permit_params :name, :box_id, :nb
-  #
-  # or
-  #
-  # permit_params do
-  #   permitted = [:name, :box_id, :nb]
-  #   permitted << :other if params[:action] == 'create' && current_user.admin?
-  #   permitted
-  # end
+#Import csv   
+ active_admin_import validate: false,
+              csv_options: {col_sep: ";" },
+              before_batch_import: ->(importer) {   
+                Prosition.where(id: importer.values_at('id')).delete_all
+              }
   
 end
